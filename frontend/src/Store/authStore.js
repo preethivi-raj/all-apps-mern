1e4e5558d49baf4d4625facf4c86e72952f05f24
PREETHIVIRAJ <preethiviraj9943@gmail.com>
2024-08-09 08:59:25 +0530

    chnage the mode for deploy

--- frontend/src/Store/authStore.js
+++ frontend/src/Store/authStore.js
@@ -1,7 +1,7 @@
 import {create} from "zustand"
 import axios from "axios"
 
-const API_URL = "/api/auth";
+const API_URL = import.meta.env.MODE==="production" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
